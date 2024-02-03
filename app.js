d45356e8c44729f88d0f3158134e3de422c43c92
Shailendra Jurel <145928630+shailendra-jurel@users.noreply.github.com>
2024-02-03 18:26:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const inputtext = document.querySelectorAll(".text-area");
+const comment = document.querySelectorAll(".right-usercomment");
+const post =    document.querySelectorAll("post-button");
+const replycomment =  document.querySelectorAll("reply-comment");
+
+
+
+
+
+
+    post.addEventListener("click", () => {
+
+            comment.innerText = inputtext;
+        
+    });
+
+    replycomment.addEventListener("click" ()=>{
+
+        var textfield = document.createElement("input");
+
+
+
+
+
+
+
+    })
+
+
+    
+
